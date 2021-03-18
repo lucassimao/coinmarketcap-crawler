14fbb62ed2a1bc37d274ce4302c11be59c0f8aff
Lucas Simão <lsimaocosta@gmail.com>
2021-03-18 11:24:19 -0300

    it improves error handling

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,22 +6,39 @@ const app = express();
 
 app.use(cors());
 
-app.get('/coins/bySupplyAvailability',async (req,res) => {
-    const size = Number(req.query.size) || 5;
-    const coins = await ElasticSearchService.listCoinsBySupplyAvailability(size); 
-    res.json(coins).end()
+app.get('/coins/bySupplyAvailability', async (req, res) => {
+    try {
+        const size = Number(req.query.size) || 5;
+        const coins = await ElasticSearchService.listCoinsBySupplyAvailability(size);
+        res.json(coins).end()
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500)
+    }
+
 })
 
-app.get('/coins/byHighestMarketDominance',async (req,res) => {
-    const size = Number(req.query.size) || 5;
-    const coins = await ElasticSearchService.listCoinsByMarketDominance(size); 
-    res.json(coins).end()
+app.get('/coins/byHighestMarketDominance', async (req, res) => {
+    try {
+        const size = Number(req.query.size) || 5;
+        const coins = await ElasticSearchService.listCoinsByMarketDominance(size);
+        res.json(coins).end()
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500)
+    }
+
 })
 
-app.get('/coins/byMaxSupply',async (req,res) => {
-    const size = Number(req.query.size) || 5;
-    const coins = await ElasticSearchService.listCoinsByMaxSupply(size); 
-    res.json(coins).end()
+app.get('/coins/byMaxSupply', async (req, res) => {
+    try {
+        const size = Number(req.query.size) || 5;
+        const coins = await ElasticSearchService.listCoinsByMaxSupply(size);
+        res.json(coins).end()
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500)
+    }
 })
 
-app.listen(process.env.PORT ,() =>  console.log(`Backend running`));
+app.listen(process.env.PORT, () => console.log(`Backend running`));
